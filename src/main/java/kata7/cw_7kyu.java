6c7aed6e26b77b14be012107be7d2697965ee5e0
crangoren <crangoren@gmail.com>
2021-09-10 20:55:37 +0300

    7kyu

--- src/main/java/kata7/cw_7kyu.java
+++ src/main/java/kata7/cw_7kyu.java
@@ -1,7 +1,25 @@
 package kata7;
 
 public class cw_7kyu {
+    /*ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
 
+    If the function is passed a valid PIN string, return true, else return false.
+
+    Examples
+"1234"   -->  true
+        "12345"  -->  false
+        "a234"   -->  false*/
+
+    public static boolean validatePin(String pin) {
+
+        String regex = "[0-9]+";
+        if ( pin.matches(regex) == true && (pin.length() == 4 || pin.length() == 6)) {
+//      int intPin = Integer.parseInt(pin.trim());
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
 
